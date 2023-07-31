165088cacfaeed7102fc9229caf541b629fc408e
Shahabhct <79043647+Shahabhct@users.noreply.github.com>
2023-07-31 10:58:46 +0400

    Unit 8.2: Navigation Stack, Strike and Toggle

--- TodoClass/ContentView.swift
+++ TodoClass/ContentView.swift
@@ -10,17 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var todos = [Todo(title: "Feed the cat"),
-                            Todo(title: "Play with cat"),
+                            Todo(title: "Play with cat", isCompleted: true),
                             Todo(title: "Get allergies"),
                             Todo(title: "Run away from cat", isCompleted: true),
                             Todo(title: "Get a new cat")
     ]
     var body: some View {
         NavigationStack {
-            List(todos) { todo in
-                Text(todo.title)
-                    .navigationTitle("Todos")
+            List($todos) { $todo in
+                HStack {
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        .onTapGesture {
+                            todo.isCompleted.toggle()
+                        }
+                    Text(todo.title)
+                        .strikethrough(todo.isCompleted)
+                    
+                   
+                }
+                    
             }
+            .navigationTitle("Todos")
         }
 
     }
