5b7f405ea45c5746672bfb9ee1deb8176ace95ee
Shahabhct <79043647+Shahabhct@users.noreply.github.com>
2023-07-31 11:44:23 +0400

    Unit 8.3 : added the detail view

--- TodoClass/ContentView.swift
+++ TodoClass/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var todos = [Todo(title: "Feed the cat", subtitle: "Feed at 12 PM every day"),
+    @State private var todos = [Todo(title: "Feed the eat", subtitle: "Feed at 12 PM every day"),
                             Todo(title: "Play with cat", isCompleted: true),
                             Todo(title: "Get allergies"),
                             Todo(title: "Run away from cat", isCompleted: true),
@@ -18,24 +18,28 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List($todos) { $todo in
-                HStack {
-                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
-                        .onTapGesture {
-                            todo.isCompleted.toggle()
-                        }
-                    VStack (alignment: .leading) {
-                        Text(todo.title)
-                            .strikethrough(todo.isCompleted)
-                        if !todo.subtitle.isEmpty{
-                            Text(todo.subtitle)
-                                .font(.caption)
-                                .foregroundColor(.gray)
+                NavigationLink {
+                    TodoDetailView(todo : $todo)
+                } label: {
+                    HStack {
+                        Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                todo.isCompleted.toggle()
+                            }
+                        VStack (alignment: .leading) {
+                            Text(todo.title)
                                 .strikethrough(todo.isCompleted)
+                            if !todo.subtitle.isEmpty{
+                                Text(todo.subtitle)
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                                    .strikethrough(todo.isCompleted)
+                            }
+                            
                         }
                         
+                       
                     }
-                    
-                   
                 }
                     
             }
---
+++ TodoClass/TodoDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  TodoDetailView.swift
+//  TodoClass
+//
+//  Created by shahab on 31/07/2023.
+//
+
+import SwiftUI
+
+struct TodoDetailView: View {
+    
+    @Binding var todo : Todo
+
+    var body: some View {
+        Form {
+            TextField("Title", text: $todo.title)
+            TextField("Subtitle", text: $todo.subtitle)
+                .foregroundColor(.gray)
+            Toggle("Completed?", isOn: $todo.isCompleted)
+        }
+        .navigationTitle("Todo Detail")
+    }
+}
+
+struct TodoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            TodoDetailView(todo: .constant(Todo(title: "feed the cdemo cat")))
+        }
+    }
+}
