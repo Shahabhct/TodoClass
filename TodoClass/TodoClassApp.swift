d273e1c9cb0c9c20bc9256ef3b0404a2edc24759
Shahabhct <79043647+Shahabhct@users.noreply.github.com>
2023-07-31 10:14:59 +0400

    Initial Commit

---
+++ TodoClass/TodoClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoClassApp.swift
+//  TodoClass
+//
+//  Created by shahab on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
